9a2c82fbc4b95672622df806037710340ea448e2
Oleg N. Cher <allot@bk.ru>
2016-06-04 18:18:16 +0300

    * OfrontOPM.cmdln.Mod: * info on Ofront+

--- Bin/build.bat
+++ Bin/build.bat
@@ -5,7 +5,7 @@
 @SET lib=-I ..\Lib\Obj ..\Lib\Ofront.a
 @SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512 %lib%
 
-%gcc% OfrontOPM.c OfrontOPS.c OfrontOPT.c OfrontOPB.c OfrontOPP.c OfrontOPC.c OfrontOPV.c OfrontCmd.c %lib% -o ..\Bin\ofront.exe
+%gcc% OfrontOPM.c OfrontOPS.c OfrontOPT.c OfrontOPB.c OfrontOPP.c OfrontOPC.c OfrontOPV.c OfrontCmd.c %lib% -o ..\Bin\ofront+.exe
 @IF errorlevel 1 PAUSE
 %gcc% OfrontOPM.c OfrontOPS.c OfrontOPT.c OfrontOPC.c OfrontOPV.c BrowserCmd.c %lib% -o ..\Bin\showdef.exe
 @IF errorlevel 1 PAUSE
