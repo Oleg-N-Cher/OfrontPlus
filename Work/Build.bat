ebcfb6564922b59e7eb844a00a0367d5dff522fb
Oleg N. Cher <allot@bk.ru>
2016-06-08 21:25:20 +0300

    * Rebuild.

--- Work/Build.bat
+++ Work/Build.bat
@@ -1,6 +1,10 @@
 @SET PATH=..\Bin;%PATH%
 @SET OBERON=%CD%;%CD%\..\Lib\Sym
 
-@ofront -m Hello.Mod
+@ofront+ -m Hello.Mod
 @IF errorlevel 1 PAUSE
-@CALL gcc Hello.c
+@CALL gcc.bat Hello.c
+
+@ofront+ -m SameFile.Mod
+@IF errorlevel 1 PAUSE
+@CALL gcc.bat SameFile.c
