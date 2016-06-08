38ad027c9e0ce0be39ac3676d9f15f215fe079e4
Oleg N. Cher <allot@bk.ru>
2016-06-08 21:25:20 +0300

    * Rebuild.

--- Bin/gcc.bat
+++ Bin/gcc.bat
@@ -7,7 +7,7 @@
 @SET WinDev=%XDev%\WinDev
 @SET PATH=%WinDev%\Bin\MinGW\bin;%PATH%
 @SET lib=-I ..\Lib\Obj
-@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--file-alignment,512 %lib%
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512 %lib%
 
 %gcc% %1 ..\Lib\Ofront.a -o %1.exe
 @IF errorlevel 1 PAUSE
--- Lib/Bin/oberon-to-c.bat
+++ Lib/Bin/oberon-to-c.bat
@@ -1,22 +1,23 @@
 @SET PATH=%CD%\..\..\Bin;%PATH%
 @CD ..\Obj
 
-ofront -se ..\Mod\SYSTEM.Mod
+ofront+ -iapxe ..\Mod\SYSTEM.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Args.Mod
+ofront+ -se ..\Mod\Args.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Console.Mod
+ofront+ -se ..\Mod\Console.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Unix.msvcrt.Mod
+ofront+ -se ..\Mod\Unix.msvcrt.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Kernel.Mod
+ofront+ -se ..\Mod\Kernel.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Files.Mod
+ofront+ -se ..\Mod\Files.Mod
+ECHO %errorlevel%
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Reals.Mod
+ofront+ -se ..\Mod\Reals.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\Modules.Mod
+ofront+ -se ..\Mod\Modules.Mod
 @IF errorlevel 1 PAUSE
-ofront -se ..\Mod\CmdlnTexts.Mod
+ofront+ -se ..\Mod\CmdlnTexts.Mod
 @IF errorlevel 1 PAUSE
-pause
+@PAUSE
