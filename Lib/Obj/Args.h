7fc8319af8b9ab0f5869f7fa044e88ce53c89b04
Oleg N. Cher <allot@bk.ru>
2016-06-20 22:35:41 +0300

    * Lib/Args: rebuild.

--- Lib/Obj/Args.h
+++ Lib/Obj/Args.h
@@ -5,13 +5,18 @@
 
 #include "SYSTEM.h"
 
+typedef
+	SYSTEM_PTR (*Args_ANYPTR)[1];
 
-import LONGINT Args_argc, Args_argv;
+
+import INTEGER Args_argc;
+import Args_ANYPTR Args_argv;
 
 
 import void Args_Get (INTEGER n, CHAR *val, LONGINT val__len);
 import void Args_GetEnv (CHAR *var, LONGINT var__len, CHAR *val, LONGINT val__len);
-import void Args_GetInt (INTEGER n, LONGINT *val);
+import void Args_GetInt (INTEGER n, INTEGER *val);
+import void Args_GetLInt (INTEGER n, LONGINT *val);
 import INTEGER Args_Pos (CHAR *s, LONGINT s__len);
 import void *Args__init(void);
 
