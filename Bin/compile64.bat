ed14cd56c77b9b2cfa3a0d2f9bdfeb613a671b45
Oleg N. Cher <allot@bk.ru>
2016-06-22 21:15:23 +0300

    * Build scripts.

---
+++ Bin/compile64.bat
@@ -0,0 +1,16 @@
+@IF NOT "%XDev%"=="" GOTO XDev
+@ECHO Please set system variable XDev=X:\Path\To\XDev
+@PAUSE
+@EXIT
+
+:XDev
+@SET WinDev=%XDev%\WinDev
+@SET PATH=%WinDev%\Bin\MinGW64\bin;%PATH%
+@SET lib=-I ..\Lib\Obj
+@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables %lib%
+
+@SET Mod=%1
+@IF %Mod%==OfrontOPM.cmdln SET Mod=OfrontOPM
+
+%gcc% -c %Mod%.c
+@IF errorlevel 1 PAUSE
