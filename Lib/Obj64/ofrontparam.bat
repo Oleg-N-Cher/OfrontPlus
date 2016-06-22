8a9463312710a6934c19ad03c221289c75be68dc
Oleg N. Cher <allot@bk.ru>
2016-06-22 21:18:16 +0300

    * Lib/ofrontparam

--- Lib/Obj64/ofrontparam.bat
+++ Lib/Obj64/ofrontparam.bat
@@ -5,8 +5,8 @@
 
 :XDev
 @SET WinDev=%XDev%\WinDev
-@SET PATH=%WinDev%\Bin\MinGW\bin;%PATH%
-@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512
+@SET PATH=%WinDev%\Bin\MinGW64\bin;%PATH%
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables
 
 %gcc% ofrontparam.c -o ofrontparam.exe
 @IF errorlevel 1 PAUSE
