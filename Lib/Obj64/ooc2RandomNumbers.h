cec001a9030c2721a522ddffccdcc6dfe8905797
Oleg N. Cher <allot@bk.ru>
2016-06-22 21:12:29 +0300

    + Lib/ooc2RandomNumbers

---
+++ Lib/Obj64/ooc2RandomNumbers.h
@@ -0,0 +1,18 @@
+/*  Ofront 1.2 -xtspkae */
+
+#ifndef ooc2RandomNumbers__h
+#define ooc2RandomNumbers__h
+
+#include "SYSTEM.h"
+
+
+
+
+import void ooc2RandomNumbers_GetSeed (INTEGER *seed);
+import void ooc2RandomNumbers_PutSeed (INTEGER seed);
+import INTEGER ooc2RandomNumbers_RND (INTEGER range);
+import REAL ooc2RandomNumbers_Random (void);
+import void *ooc2RandomNumbers__init(void);
+
+
+#endif
