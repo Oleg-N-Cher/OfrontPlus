2a0f4b6b5540bb921e8fe369027eeda05a2c1432
Oleg N. Cher <allot@bk.ru>
2016-06-22 21:15:23 +0300

    * Build scripts.

---
+++ Lib/Bin/mingw64.bat
@@ -0,0 +1,12 @@
+@REM args:
+@REM   LibName ModName
+
+@SET WinDev=%XDev%\WinDev
+@SET PATH=%WinDev%\Bin\MinGW64\bin;%PATH%
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables
+
+%gcc% -c %2.c -I "."
+@IF errorlevel 1 PAUSE
+
+@ar.exe -rc %1 %2.o
+@DEL %2.o
