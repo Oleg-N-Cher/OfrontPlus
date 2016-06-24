3f20159c6faf48bf7c2975227d0ccbc9d494d243
Oleg N. Cher <allot@bk.ru>
2016-06-24 04:07:32 +0300

    * Lib/Obj/SYSTEM => Lib/C

--- Bin/gcc64.bat
+++ Bin/gcc64.bat
@@ -1,16 +1,13 @@
-@IF NOT "%XDev%"=="" GOTO XDev
+@IF NOT /%XDev%==/ GOTO XDev
 @ECHO Please set system variable XDev=X:\Path\To\XDev
 @PAUSE
-@EXIT
 
 :XDev
+
 @SET WinDev=%XDev%\WinDev
 @SET PATH=%WinDev%\Bin\MinGW64\bin;%PATH%
 @SET lib=-I ..\Lib\Obj64 -I ..\Lib\C
-@SET gcc=gcc.exe -Os -fno-exceptions -fno-asynchronous-unwind-tables %lib%
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512 %lib%
 
-@SET Mod=%1
-@IF %Mod%==OfrontOPM.cmdln SET Mod=OfrontOPM
-
-%gcc% -c %Mod%.c
+%gcc% %1 ..\Lib\Ofront64.a -o %1.exe
 @IF errorlevel 1 PAUSE
