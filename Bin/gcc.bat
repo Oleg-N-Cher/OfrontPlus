bd1793a3e3f20c542ba3791af5eee25d3b34d39b
Oleg N. Cher <allot@bk.ru>
2016-06-27 18:18:51 +0300

    * Build scripts.

--- Bin/gcc.bat
+++ Bin/gcc.bat
@@ -1,12 +1,11 @@
 @IF NOT /%XDev%==/ GOTO XDev
 @ECHO Please set system variable XDev=X:\Path\To\XDev
 @PAUSE
+@EXIT
 
 :XDev
-
-@SET WinDev=%XDev%\WinDev
-@SET PATH=%WinDev%\Bin\MinGW\bin;%PATH%
-@SET lib=-I ..\Lib\Obj -I ..\Lib\C -I ..\Obj -I ..\C
+@SET PATH=%XDev%\WinDev\Bin\MinGW\bin;%PATH%
+@SET lib=-I ..\Lib\Obj -I ..\Lib\C ..\Lib\Ofront.a
 @SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512 %lib%
 
 %gcc% %1 %2 %3 %4 %5 %6 %7 %8 %9
