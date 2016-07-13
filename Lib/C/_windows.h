ff596b1946479ae8a94e428e00bd2025d23e9961
Oleg N. Cher <allot@bk.ru>
2016-07-13 19:22:19 +0300

    * Platform.Windows: made compilable.

---
+++ Lib/C/_windows.h
@@ -0,0 +1,10 @@
+// windows.h wrapper
+// Includes winsock.h while avoiding conflicts with Oberon types.
+
+#define BOOLEAN _BOOLEAN
+#define BYTE _BYTE
+#define CHAR _CHAR
+#include <windows.h>
+#undef BOOLEAN
+#undef BYTE
+#undef CHAR
