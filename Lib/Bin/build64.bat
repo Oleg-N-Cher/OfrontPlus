89dbd8a281cf7b57827558c123894409f7f7be02
Oleg N. Cher <allot@bk.ru>
2016-07-13 19:12:48 +0300

    + Lib/Mod/Platform*.Mod by David C W Brown

--- Lib/Bin/build64.bat
+++ Lib/Bin/build64.bat
@@ -15,6 +15,7 @@ pause
 @CALL ..\Bin\mingw64 %Lib% Args
 pause
 @CALL ..\Bin\mingw64 %Lib% Console
+@CALL ..\Bin\mingw %Lib% Platform
 @CALL ..\Bin\mingw64 %Lib% Unix
 @CALL ..\Bin\mingw64 %Lib% Kernel
 @CALL ..\Bin\mingw64 %Lib% Files
