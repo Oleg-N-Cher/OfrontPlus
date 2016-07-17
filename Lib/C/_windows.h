579115ee6a9477da292c99c416f1d8597597d415
Oleg N. Cher <allot@bk.ru>
2016-07-17 08:24:23 +0300

    * Lib/Platform: * problem with GetFileSizeEx

--- Lib/C/_windows.h
+++ Lib/C/_windows.h
@@ -4,7 +4,13 @@
 #define BOOLEAN _BOOLEAN
 #define BYTE _BYTE
 #define CHAR _CHAR
+
+#undef _WIN32_WINNT
+// 0x0501 is for Windows XP ( no service pack ), 
+// values for other configurations are listed in the article
+#define _WIN32_WINNT 0x0501
 #include <windows.h>
+
 #undef BOOLEAN
 #undef BYTE
 #undef CHAR
