f7ccdd9b405780207fc63c3ebc9ea8638664d738
Oleg N. Cher <allot@bk.ru>
2016-07-17 08:02:28 +0300

    + Lib/ Files, Heap & Strings from voc (by David C W Brown).

--- Lib/Bin/oberon-to-c.bat
+++ Lib/Bin/oberon-to-c.bat
@@ -7,6 +7,10 @@ ofront+ -lse ..\Mod\Args.Mod
 @IF errorlevel 1 PAUSE
 ofront+ -lse ..\Mod\Console.Mod
 @IF errorlevel 1 PAUSE
+ofront+ -lse ..\Mod\Platform.Mod
+@IF errorlevel 1 PAUSE
+ofront+ -liapxe ..\Mod\Heap.Mod
+@IF errorlevel 1 PAUSE
 ofront+ -lse ..\Mod\Unix.msvcrt.Mod
 @IF errorlevel 1 PAUSE
 ofront+ -lse ..\Mod\Kernel.Mod
