4d8f35f62f3896580ef34e9da5814dd7ac9a787c
Oleg N. Cher <allot@bk.ru>
2016-07-22 13:08:08 +0300

    + Lib/Test*/TestFiles

---
+++ Lib/Test64/TestFiles.bat
@@ -0,0 +1,9 @@
+@SET PATH=..\Bin;..\..\Bin;%PATH%
+@SET OBERON=%CD%;%CD%\..\Sym64
+
+@ofront+ -m TestFiles.Mod
+@IF errorlevel 1 PAUSE
+@CALL gcc64.bat TestFiles.c -o TestFiles.exe
+
+@IF EXIST TestFiles.exe TestFiles.exe
+@PAUSE
