25d3495766e2c0db3621ff27b6b95a24a2c6a6c5
Oleg N. Cher <allot@bk.ru>
2016-07-31 20:52:00 +0300

    - Lib/Test32L64

--- Lib/Test32L64/TestConsole.bat
+++
@@ -1,9 +0,0 @@
-@SET PATH=..\Bin;..\..\Bin;%PATH%
-@SET OBERON=%CD%;%CD%\..\Sym32L64
-
-@ofront+ -m ..\Test\TestConsole.Mod
-@IF errorlevel 1 PAUSE
-@CALL gcc32L64.bat TestConsole.c -o TestConsole.exe
-
-@IF EXIST TestConsole.exe TestConsole.exe
-@PAUSE
