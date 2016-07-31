c3e28ace3e36c6f27f5cb53ebb53dc015d839b38
Oleg N. Cher <allot@bk.ru>
2016-07-31 20:52:00 +0300

    - Lib/Test32L64

--- Lib/Test32L64/TestArgs.bat
+++
@@ -1,9 +0,0 @@
-@SET PATH=..\Bin;..\..\Bin;%PATH%
-@SET OBERON=%CD%;%CD%\..\Sym32L64
-
-@ofront+ -m ..\Test\TestArgs.Mod
-@IF errorlevel 1 PAUSE
-@CALL gcc32L64.bat TestArgs.c -o TestArgs.exe
-
-@IF EXIST TestArgs.exe TestArgs.exe arg1 "ARG 2" "__!@#$_arg3_&%*()-="
-@PAUSE
--- Lib/Test32L64/TestOoc2RandomNumbers.bat
+++
@@ -1,9 +0,0 @@
-@SET PATH=..\Bin;..\..\Bin;%PATH%
-@SET OBERON=%CD%;%CD%\..\Sym32L64
-
-@ofront+ -m ..\Test\TestOoc2RandomNumbers.Mod
-@IF errorlevel 1 PAUSE
-@CALL gcc32L64.bat TestOoc2RandomNumbers.c -o TestOoc2RandomNumbers.exe
-
-@IF EXIST TestOoc2RandomNumbers.exe TestOoc2RandomNumbers.exe
-@PAUSE
