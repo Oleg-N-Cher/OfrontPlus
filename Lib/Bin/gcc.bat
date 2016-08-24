f4d8c28a2c6c059cccbdb978b9f4283c092d2162
Oleg N. Cher <allot@bk.ru>
2016-08-24 17:19:32 +0300

    * Rebuild.

--- Lib/Bin/gcc.bat
+++ Lib/Bin/gcc.bat
@@ -6,7 +6,7 @@
 :XDev
 @SET PATH=%XDev%\WinDev\Bin\MinGW\bin;%PATH%
 @SET lib=-I ..\Obj -I ..\C ..\Ofront.a
-@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--relax -Wl,--file-alignment,512 -Wl,--strip-all
 
 %gcc% %1 %2 %3 %4 %5 %6 %7 %8 %9 %lib% 2>&1|more
 @IF errorlevel 1 PAUSE
