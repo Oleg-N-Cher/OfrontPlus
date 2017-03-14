cb13e286c1c0cfa03ae57ea5c27bbc7a9b207e0e
Oleg N. Cher <allot@bk.ru>
2017-03-15 01:55:05 +0200

    * Bin/ & Lib/Bin/ oberon-to-c*.bat

--- Lib/Bin/oberon-to-c64.bat
+++ Lib/Bin/oberon-to-c64.bat
@@ -1,37 +1,14 @@
-@SET PATH=%CD%\..\..\Bin64;%PATH%
-@CD ..\Obj64
-@SET OBERON=%CD%;%CD%\..\Sym64
+@ECHO OFF
+SET PATH=%CD%\..\..\Bin64
+CD ..\Obj64
+SET OBERON=%CD%;%CD%\..\Sym64;%CD%\..\Mod
 
 :: Ofront+
-::ofront+ -apxe Heap0.Mod
-::@IF errorlevel 1 PAUSE
-ofront+ -seatpx ..\Mod\Platform.Windows.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Console.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -seatpx ..\Mod\Kernel.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Args.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Strings.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Files.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Reals.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Modules.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Texts.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\Oberon.Mod
-@IF errorlevel 1 PAUSE
+::ofront+ -apx Heap0.Mod
+::IF errorlevel 1 PAUSE
+ofront+ Platform.Windows.Mod -eatpx Console.Mod Kernel.Mod -eatpx Args.Mod Strings.Mod Files.Mod Reals.Mod Modules.Mod Texts.Mod Oberon.Mod
+IF errorlevel 1 PAUSE
 
 :: OOC2
-ofront+ -se ..\Mod\ooc2Ascii.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\ooc2ConvTypes.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\ooc2RandomNumbers.Mod
-@IF errorlevel 1 PAUSE
-ofront+ -se ..\Mod\ooc2Strings.Mod
-@IF errorlevel 1 PAUSE
+ofront+ ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
+IF errorlevel 1 PAUSE
