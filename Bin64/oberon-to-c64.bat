c011028531bee3fbee52d90855d44227cbdbcbf1
Oleg N. Cher <allot@bk.ru>
2017-03-15 01:55:05 +0200

    * Bin/ & Lib/Bin/ oberon-to-c*.bat

--- Bin64/oberon-to-c64.bat
+++ Bin64/oberon-to-c64.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET PATH=%CD%
-CD ..\Obj
-SET OBERON=%CD%;%CD%\..\Lib\Sym;%CD%\..\Sym;%CD%\..\Mod
+CD ..\Obj64
+SET OBERON=%CD%;%CD%\..\Lib\Sym64;%CD%\..\Sym64;%CD%\..\Mod
 
 ofront+ -e OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
