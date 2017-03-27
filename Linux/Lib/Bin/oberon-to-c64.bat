b5ef517040b6234f0497588e935388274e5e051c
Oleg N. Cher <allot@bk.ru>
2017-03-27 18:53:58 +0300

    * Linux: * build scripts.

---
+++ Linux/Lib/Bin/oberon-to-c64.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET PATH=%CD%\..\..\..\Bin64
+CD ..\Obj64
+SET OBERON=%CD%;%CD%\..\Sym64;%CD%\..\..\..\Lib\Mod
+
+:: Ofront+
+ofront+ -s Heap64.Mod -apx Platform.Unix.Mod -atpx Console.Mod Kernel.Mod -atpx Args.Mod Strings.Mod Files.Mod Reals.Mod Modules.Mod Texts.Mod Oberon.Mod
+IF errorlevel 1 PAUSE
+
+:: OOC2
+ofront+ -e ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
+IF errorlevel 1 PAUSE
+
+MOVE /Y *.sym ..\Sym64
