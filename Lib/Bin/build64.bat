97380b4991781cb218c902b9d76af65d60c94efe
Oleg N. Cher <allot@bk.ru>
2017-04-13 03:03:16 +0300

    * Lib: * cc*, o2c*, - Lib/C, - ofrontparam.

--- Lib/Bin/build64.bat
+++ Lib/Bin/build64.bat
@@ -1,32 +1,3 @@
-@IF NOT "%XDev%"=="" GOTO XDev
-@ECHO Please set system variable XDev=X:\Path\To\XDev
-@PAUSE
-@EXIT
-
-:XDev
-@IF "%1"=="WinApi" DEL WinApi.c
-
-@SET Lib=..\Ofront64.a
-@IF EXIST %Lib% DEL %Lib%
-
-:: Ofront+
-@CALL ..\Bin\mingw64 %Lib% SYSTEM
-@CALL ..\Bin\mingw64 %Lib% Heap
-@CALL ..\Bin\mingw64 %Lib% Platform
-@CALL ..\Bin\mingw64 %Lib% Console
-@CALL ..\Bin\mingw64 %Lib% Kernel
-@CALL ..\Bin\mingw64 %Lib% Args
-@CALL ..\Bin\mingw64 %Lib% Strings
-@CALL ..\Bin\mingw64 %Lib% Files
-@CALL ..\Bin\mingw64 %Lib% Reals
-@CALL ..\Bin\mingw64 %Lib% Modules
-@CALL ..\Bin\mingw64 %Lib% Texts
-@CALL ..\Bin\mingw64 %Lib% Oberon
-
-:: OOC2
-@CALL ..\Bin\mingw64 %Lib% ooc2Ascii
-@CALL ..\Bin\mingw64 %Lib% ooc2ConvTypes
-@CALL ..\Bin\mingw64 %Lib% ooc2RandomNumbers
-@CALL ..\Bin\mingw64 %Lib% ooc2Strings
-
-@..\Bin\clear
+@CALL o2c64.bat
+@CALL ..\Bin\cc64.bat
+@DEL *.o
