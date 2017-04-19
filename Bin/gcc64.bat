74437df4f10b507b0b58039f4d126d98a5d0c9b5
Oleg N. Cher <allot@bk.ru>
2017-04-20 01:49:50 +0300

    * Bin/*.bat

--- Bin/gcc64.bat
+++ Bin/gcc64.bat
@@ -5,7 +5,7 @@
 
 :XDev
 @SET PATH=%XDev%\WinDev\Bin\MinGW64\bin;%PATH%
-@SET lib=-I ..\Lib\Obj64 -I ..\Lib\C ..\Lib\Ofront64.a
+@SET lib=-I ..\Lib\Obj64 -I ..\Lib\Mod ..\Lib\Ofront64.a
 @SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512 %lib%
 
 %gcc% %1 %2 %3 %4 %5 %6 %7 %8 %9
