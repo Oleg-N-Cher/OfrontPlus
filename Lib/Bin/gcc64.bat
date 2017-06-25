e91c07f899c36857295d789bdbde97a412e0f682
Oleg N. Cher <allot@bk.ru>
2017-06-25 05:32:13 +0300

    issue #33: / Lib/Mod/Platform.Windows.FileSize: GetFileSizeEx => GetFileSize (work under Windows 9x).

---
+++ Lib/Bin/gcc64.bat
@@ -0,0 +1,12 @@
+@IF NOT /%XDev%==/ GOTO XDev
+@ECHO Please set system variable XDev=X:\Path\To\XDev
+@PAUSE
+@EXIT
+
+:XDev
+@SET PATH=%XDev%\WinDev\Bin\MinGW64\bin;%PATH%
+@SET lib=-I ..\Obj64 -I ..\Mod ..\Ofront64.a
+@SET gcc=gcc.exe -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections -Wl,--file-alignment,512
+
+%gcc% %1 %2 %3 %4 %5 %6 %7 %8 %9 %lib%
+@IF errorlevel 1 PAUSE
---
+++ Lib/Test64/TestFileSize.bat
@@ -0,0 +1,8 @@
+@SET PATH=..\Bin;..\..\Bin64;%PATH%
+@SET OBERON=%CD%;%CD%\..\Sym64
+
+@ofront+ -m ..\Test\TestFileSize.Mod
+@IF errorlevel 1 PAUSE
+@CALL gcc64.bat TestFileSize.c -o TestFileSize.exe
+
+@PAUSE
