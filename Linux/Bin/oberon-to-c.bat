e101f8ce1019ab7017bbce92b8e242c1ca8a4eaa
Oleg N. Cher <allot@bk.ru>
2017-12-01 11:04:14 +0200

    * Build script.

--- Linux/Bin/oberon-to-c.bat
+++ Linux/Bin/oberon-to-c.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 SET PATH=%CD%\..\..\Bin
 CD ..\Obj
-SET OBERON=%CD%;%CD%\..\..\Lib\Sym;%CD%\..\Sym;%CD%\..\..\Mod
+SET OBERON=%CD%;%CD%\..\Lib\Sym;%CD%\..\Sym;%CD%\..\..\Mod
 
 ofront+ -e OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
