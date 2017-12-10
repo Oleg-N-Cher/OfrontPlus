ebcbd0e32d4a0072105bb73f417ed7da614842ab
Zorko@CentOS <stef@anarxi.st>
2017-12-10 20:50:54 +0200

    + Linux/Samples64

---
+++ Linux/Samples64/Cnezinka.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH="$PATH:../Bin64"
+export OBERON=.:../Lib/Sym64
+export CC="gcc -I . -I../../Lib/Mod -I../Lib/Obj64 -L../Lib -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections"
+
+ofront+ -m ../../Samples/Turtle/Cnezinka.Mod
+if [ $? != 0 ]; then exit 1; fi
+$CC Cnezinka.c -o Cnezinka -lOfront64 -ldl -I/usr/include/SDL2 -L/usr/lib -lSDL2 -lSDL2_image
+if [ $? != 0 ]; then exit 1; fi
