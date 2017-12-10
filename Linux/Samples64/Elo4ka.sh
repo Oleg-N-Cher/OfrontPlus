0b6e379283ba4b9980a17bb2f020451b5b2b05dc
Zorko@CentOS <stef@anarxi.st>
2017-12-10 20:50:54 +0200

    + Linux/Samples64

---
+++ Linux/Samples64/Elo4ka.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH="$PATH:../Bin64"
+export OBERON=.:../Lib/Sym64
+export CC="gcc -I . -I../../Lib/Mod -I../Lib/Obj64 -L../Lib -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections"
+
+ofront+ -m ../../Samples/Elo4ka.Mod
+if [ $? != 0 ]; then exit 1; fi
+$CC Elo4ka.c -o Elo4ka -lOfront64 -ldl -I/usr/include/SDL2 -L/usr/lib -lSDL2 -lSDL2_image
+if [ $? != 0 ]; then exit 1; fi
