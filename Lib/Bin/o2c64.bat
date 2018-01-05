d42c01bf546a3c6b5b1c12631be6320c44249f93
Oleg N. Cher <allot@bk.ru>
2018-01-05 05:35:16 +0200

    + Lib/Sound

--- Lib/Bin/o2c64.bat
+++ Lib/Bin/o2c64.bat
@@ -13,9 +13,14 @@ IF errorlevel 1 PAUSE
 ofront+ -eC -88 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
 IF errorlevel 1 PAUSE
 
+:: POW
+
+ofront+ -eC -88 powStrings.Mod
+IF errorlevel 1 PAUSE
+
 :: Free Oberon
 
-ofront+ -sC -88 SDL2.Mod Out.Mod Graph.Mod Math.Mod Turtle.Mod
+ofront+ -sC -88 SDL2.Mod SDL2mixer.Mod Out.Mod Graph.Mod Math.Mod Sound.Mod Turtle.Mod
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym64 >NUL
