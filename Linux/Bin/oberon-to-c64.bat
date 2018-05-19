5e78f3e45027977b957176a9d1ae07cea413b467
Oleg N. Cher <allot@bk.ru>
2018-05-20 01:50:57 +0300

    * Linux build scrtpts.

--- Linux/Bin/oberon-to-c64.bat
+++ Linux/Bin/oberon-to-c64.bat
@@ -3,7 +3,7 @@ SET PATH=%CD%\..\..\Bin
 CD ..\Obj64
 SET OBERON=%CD%;%CD%\..\Lib\Sym64;%CD%\..\Sym64;%CD%\..\..\Mod
 
-ofront+ -eC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
+ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym64 >NUL
--- Linux/Lib/Bin/o2c.bat
+++ Linux/Lib/Bin/o2c.bat
@@ -11,9 +11,16 @@ IF errorlevel 1 PAUSE
 ofront+ -eC -44 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
 IF errorlevel 1 PAUSE
 
-:: Free Oberon
+:: POW
+ofront+ -eC -44 powStrings.Mod
+IF errorlevel 1 PAUSE
 
-ofront+ -sC -44 SDL2.Mod Out.Mod Graph.Mod Math.Mod Turtle.Mod
+:: Free Oberon
+ofront+ -sC -44 SDL2.Mod SDL2mixer.Mod Out.Mod Graph.Mod Math.Mod Sound.Mod Turtle.Mod
+IF errorlevel 1 PAUSE
+
+:: libCurl
+ofront+ -sC -44 libCurl.Mod NetHttp.Mod
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
