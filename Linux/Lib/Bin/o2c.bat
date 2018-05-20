5cef2fdd054ccfc88c7269d3a5182a9abb936e15
Oleg N. Cher <zorko@zorko.home>
2018-05-20 02:46:01 +0000

    * Linux: * build scripts.

--- Linux/Lib/Bin/o2c.bat
+++ Linux/Lib/Bin/o2c.bat
@@ -4,22 +4,27 @@ CD ..\Obj
 SET OBERON=%CD%;%CD%\..\Sym;%CD%\..\..\..\Lib\Mod
 
 :: Ofront+
+
 ofront+ -sC -44 Heap32.Mod -apx Types32.Mod Platform.Unix.Mod -atpx Console.Mod Kernel.Mod -atpx Args.Mod Strings.Mod Files.Mod Reals.Mod Modules.Mod Texts.Mod Oberon.Mod
 IF errorlevel 1 PAUSE
 
 :: OOC2
+
 ofront+ -eC -44 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
 IF errorlevel 1 PAUSE
 
 :: POW
+
 ofront+ -eC -44 powStrings.Mod
 IF errorlevel 1 PAUSE
 
 :: Free Oberon
+
 ofront+ -sC -44 SDL2.Mod SDL2mixer.Mod Out.Mod Graph.Mod Math.Mod Sound.Mod Turtle.Mod
 IF errorlevel 1 PAUSE
 
 :: libCurl
+
 ofront+ -sC -44 libCurl.Mod NetHttp.Mod
 IF errorlevel 1 PAUSE
 
--- Linux/Lib/Bin/o2c64.bat
+++ Linux/Lib/Bin/o2c64.bat
@@ -4,22 +4,27 @@ CD ..\Obj64
 SET OBERON=%CD%;%CD%\..\Sym64;%CD%\..\..\..\Lib\Mod
 
 :: Ofront+
+
 ofront+ -sC -88 Heap64.Mod -apx Types64.Mod Platform.Unix.Mod -atpx Console.Mod Kernel.Mod -atpx Args.Mod Strings.Mod Files.Mod Reals.Mod Modules.Mod Texts.Mod Oberon.Mod
 IF errorlevel 1 PAUSE
 
 :: OOC2
+
 ofront+ -eC -88 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
 IF errorlevel 1 PAUSE
 
 :: POW
+
 ofront+ -eC -88 powStrings.Mod
 IF errorlevel 1 PAUSE
 
 :: Free Oberon
+
 ofront+ -sC -88 SDL2.Mod SDL2mixer.Mod Out.Mod Graph.Mod Math.Mod Sound.Mod Turtle.Mod
 IF errorlevel 1 PAUSE
 
 :: libCurl
+
 ofront+ -sC -88 libCurl.Mod NetHttp.Mod
 IF errorlevel 1 PAUSE
 
