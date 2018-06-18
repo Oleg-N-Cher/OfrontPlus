049181e8209c1e590dc818602574506fadc7bede
Oleg N. Cher <allot@bk.ru>
2018-06-18 18:03:30 +0300

    * Lib/Bin/cc*.bat

--- Lib/Bin/cc.bat
+++ Lib/Bin/cc.bat
@@ -4,11 +4,14 @@ ECHO Please set system variable XDev=X:\Path\To\XDev
 PAUSE
 EXIT
 
+::SET Aliasing=-fno-strict-aliasing
+SET Aliasing=-Wstrict-aliasing
+
 :XDev
 
 CD ..\Obj
 SET PATH=%XDev%\WinDev\Bin\MinGW\bin
-SET CC=gcc.exe -I. -I..\Mod -m32 -s -Os -g0 -fvisibility=hidden -finline-small-functions -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections -c
+SET CC=gcc.exe -I. -I..\Mod -m32 -s -Os -g0 -fvisibility=hidden -finline-small-functions -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables -ffunction-sections %Aliasing% -c
 SET AR=ar.exe -rc ..\Ofront.a
 IF EXIST ..\Ofront.a DEL ..\Ofront.a
 
@@ -32,9 +35,9 @@ IF errorlevel 1 PAUSE
 
 :: Free Oberon
 
-%CC% SDL2.c SDL2mixer.c Out.c Graph.c Math.c Sound.c Turtle.c -w
+%CC% SDL2.c SDL2mixer.c Out.c Graph.c Math.c MathL.c Sound.c Turtle.c -w
 IF errorlevel 1 PAUSE
-%AR% SDL2.o SDL2mixer.o Out.o Graph.o Math.o Sound.o Turtle.o
+%AR% SDL2.o SDL2mixer.o Out.o Graph.o Math.o MathL.c Sound.o Turtle.o
 
 :: libCurl
 
