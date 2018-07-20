d9fafc9ddbe514a3aac338f645c1a00cd48133e3
Oleg N. Cher <allot@bk.ru>
2018-07-20 05:42:27 +0300

    + Linux_arm

---
+++ Linux_arm/Lib/Bin/o2c.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+SET PATH=%CD%\..\..\..\Bin
+CD ..\Obj
+SET OBERON=%CD%;%CD%\..\Sym;%CD%\..\..\..\Lib\Mod
+
+:: Ofront+
+
+ofront+ -sC -48 Heap32.Mod -apx Types32.Mod Platform.Unix.Mod -atpx Console.Mod Kernel.Mod -atpx Args.Mod Strings.Mod Files.Mod Reals.Mod Modules.Mod Texts.Mod Oberon.Mod
+IF errorlevel 1 PAUSE
+
+:: OOC2
+
+ofront+ -eC -48 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
+IF errorlevel 1 PAUSE
+
+:: POW
+
+ofront+ -eC -48 powStrings.Mod
+IF errorlevel 1 PAUSE
+
+:: Free Oberon
+
+ofront+ -sC -48 SDL2.Mod SDL2mixer.Mod Out.Mod Graph.Mod Math.Mod Sound.Mod Turtle.Mod
+IF errorlevel 1 PAUSE
+
+:: libCurl
+
+ofront+ -sC -48 libCurl.Mod NetHttp.Mod
+IF errorlevel 1 PAUSE
+
+FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
