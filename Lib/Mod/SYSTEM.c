130742533820a5aa604ed5eed8ce40adb39213dd
Oleg N. Cher <allot@bk.ru>
2019-01-05 12:28:00 +0200

    * issue #54: SYSTEM_DIV(MIN(LONGINT), n): fix error with overflow.

--- Lib/Mod/SYSTEM.c
+++ Lib/Mod/SYSTEM.c
@@ -39,13 +39,8 @@ double  SYSTEM_ABSD(double i)              {return __ABS(i);}
 
 LONGINT SYSTEM_DIV(LONGINT x, LONGINT y)
 {
-  if (x == 0) return 0;
-  if (x >= 0)
-    if (y >= 0) {return x/y;}
-    else        {return -((x-y-1)/(-y));}
-  else
-    if (y >= 0) {return -((y-x-1)/y);}
-    else        {return (-x)/(-y);}
+  if (x >= 0) return x / y;
+  return -1 - (-1-x) / y;
 }
 
 LONGINT SYSTEM_MOD(LONGINT x, LONGINT y)
