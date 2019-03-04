6baee9d94aae5aaaead38ba676ea6972ca455ae4
Zorko@Opi <allot@bk.ru>
2019-03-04 12:23:22 +0200

    * Linux_arm/Examples

--- Linux_arm/Samples/Mandelbrot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH="$PATH:../Bin"
-export OBERON=.:../Lib/Sym
-export CC="gcc -I . -I../../Lib/Mod -I../Lib/Obj -L../Lib -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections"
-
-ofront+ -m ../../Samples/Mandelbrot.Mod
-if [ $? != 0 ]; then exit 1; fi
-$CC Mandelbrot.c -o Mandelbrot -lOfront -ldl -I/usr/include/SDL2 -L/usr/lib -lSDL2 -lSDL2_image
-if [ $? != 0 ]; then exit 1; fi
