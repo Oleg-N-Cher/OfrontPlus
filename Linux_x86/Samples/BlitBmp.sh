c480bf10bbe5e6c87823d3cb7d3573bb299e0106
Oleg N. Cher <zorko@zorko.home>
2019-03-06 23:35:25 +0000

    * Linux_x86: upd. Examples & rebuild.

--- Linux_x86/Samples/BlitBmp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PATH="$PATH:../Bin"
-export OBERON=.:../Lib/Sym
-export CC="gcc -I . -I../../Lib/Mod -I../Lib/Obj -L../Lib -s -Os -fno-exceptions -fno-asynchronous-unwind-tables -Wl,--gc-sections"
-
-ofront+ -m ../../Samples/BlitBmp.Mod
-if [ $? != 0 ]; then exit 1; fi
-$CC BlitBmp.c -o BlitBmp -lOfront -ldl -I/usr/include/SDL2 -L/usr/lib -lSDL2 -lSDL2_image
-if [ $? != 0 ]; then exit 1; fi
