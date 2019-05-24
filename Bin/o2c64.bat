8f47af0fdd0a868fb8bd9cb9f1ca96ca8b90b3aa
Oleg N. Cher <allot@bk.ru>
2019-05-24 03:50:24 +0300

    * ImportGraph

--- Bin/o2c64.bat
+++ Bin/o2c64.bat
@@ -3,7 +3,7 @@ CD ..\Obj64
 SET PATH=..\Bin;%PATH%
 SET OBERON=.\;.\..\Lib\Sym64;.\..\Sym64;.\..\Mod
 
-ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
+ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym64 >NUL
