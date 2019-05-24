539ec94412647883128c937abe5f0caef2b085f7
Oleg N. Cher <allot@bk.ru>
2019-05-24 03:50:24 +0300

    * ImportGraph

--- Bin/o2c.bat
+++ Bin/o2c.bat
@@ -3,7 +3,7 @@ CD ..\Obj
 SET PATH=..\Bin;%PATH%
 SET OBERON=.\;.\..\Lib\Sym;.\..\Sym;.\..\Mod
 
-ofront+ -sC -48 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
+ofront+ -sC -48 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
