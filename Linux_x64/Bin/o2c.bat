5c3326928ff1e7f47d9aeb4afb1c998fdc312784
Zorko@Opi <allot@bk.ru>
2019-08-17 12:40:49 +0300

    * Linux_arm: rebuild.

--- Linux_x64/Bin/o2c.bat
+++ Linux_x64/Bin/o2c.bat
@@ -3,7 +3,7 @@ SET PATH=%CD%\..\..\Bin
 CD ..\Obj
 SET OBERON=%CD%;%CD%\..\Lib\Sym;%CD%\..\Sym;%CD%\..\..\Mod
 
-ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
+ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
