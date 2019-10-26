17565164bf72052c06e3c8bd4735eca8ba61b3c0
Oleg N. Cher <allot@bk.ru>
2019-10-26 03:56:34 +0300

    + SYSTEM.COPY procedure (as in Oberon/Oberon-2) to -3|7|C dialects.

--- Lib/Test/TestASH.bat
+++ Lib/Test/TestASH.bat
@@ -1,6 +1,6 @@
 @SET PATH=..\Bin;..\..\Bin;%PATH%
 @SET OBERON=%CD%;%CD%\..\Sym
 
-@ofront+ -mC -48 TestASH.Mod
+@ofront+ -m3 -48 TestASH.Mod
 @IF errorlevel 1 PAUSE
 @CALL gcc.bat TestASH.c -o TestASH.exe
