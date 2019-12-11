bde5d1221604fa367f90c0c694507fecb0c20d78
Oleg N. Cher <allot@bk.ru>
2019-12-11 03:20:27 +0200

    + Target/BlackBox

---
+++ Target/BlackBox/+Text.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+IF NOT "%XDev%"=="" GOTO XDev
+ECHO Please set system variable XDev=X:\Path\To\XDev
+PAUSE
+EXIT
+
+:XDev
+SET txt=%XDev%\Bin\odc2txt.exe
+%txt% Mod\Browser.odc Mod\Text\Browser.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\Cmd.odc Mod\Text\Cmd.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPB.odc Mod\Text\OPB.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPC.odc Mod\Text\OPC.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPM.odc Mod\Text\OPM.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPP.odc Mod\Text\OPP.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPS.odc ..\..\Mod\OfrontOPS.Mod
+IF errorlevel 1 PAUSE
+%txt% Mod\OPT.odc Mod\Text\OPT.txt
+IF errorlevel 1 PAUSE
+%txt% Mod\OPV.odc Mod\Text\OPV.txt
+IF errorlevel 1 PAUSE
+%txt% Rsrc\Strings.odc Rsrc\Text\Strings.txt
+IF errorlevel 1 PAUSE
