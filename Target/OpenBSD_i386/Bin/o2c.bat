becde1e867782cd80f7b0e7f053f780915f64508
Oleg N. Cher <allot@bk.ru>
2020-04-17 05:47:39 +0300

    + OpenBSD*/*/Bin/o2c.bat

---
+++ Target/OpenBSD_i386/Bin/o2c.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET PATH=%CD%\..\..\Win32
+CD ..\Obj
+SET OBERON=%CD%;%CD%\..\Lib\Sym;%CD%\..\Sym;%CD%\..\..\..\Mod
+
+ofront+ -sC -44 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m
+IF errorlevel 1 PAUSE
+
+FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
---
+++ Target/OpenBSD_i386/Lib/Bin/o2c.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+SET PATH=%CD%\..\..\..\Win32
+CD ..\Obj
+SET OBERON=%CD%;%CD%\..\Sym;%CD%\..\..\..\..\Mod\Lib
+
+:: Ofront+
+
+ofront+ -sC -44 Heap.Mod -apx Platform.Unix.Mod -atpx Console.Mod Kernel.Mod -atpx Args.Mod Reals.Mod Strings.Mod Files.Mod Modules.Mod Texts.Mod Oberon.Mod
+IF errorlevel 1 PAUSE
+
+:: Eco
+
+ofront+ -sC -44 ecoBigSets.Mod ecoBuffer.Mod ecoListen.Mod ecoLists.Mod ecoParser.Mod
+IF errorlevel 1 PAUSE
+
+:: OOC2
+
+ofront+ -eC -44 ooc2Ascii.Mod ooc2ConvTypes.Mod ooc2RandomNumbers.Mod ooc2Strings.Mod
+IF errorlevel 1 PAUSE
+
+:: POW
+
+ofront+ -eC -44 powStrings.Mod
+IF errorlevel 1 PAUSE
+
+:: Free Oberon
+
+ofront+ -sC -44 SDL2.Mod SDL2mixer.Mod SQLite.Mod Out.Mod In.Mod Graph.Mod Math.Mod MathL.Mod Sound.Mod Turtle.Mod
+IF errorlevel 1 PAUSE
+
+:: libCurl
+
+ofront+ -sC -44 libCurl.Mod NetHttp.Mod
+IF errorlevel 1 PAUSE
+
+FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
