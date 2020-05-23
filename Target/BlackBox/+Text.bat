571f220cd3aa2bd10f30d84f6e32d2c94d875c3f
Oleg N. Cher <allot@bk.ru>
2020-05-23 06:19:15 +0300

    * Migrated: OPB, OPC, OPP, OPT & OPV; Constant arrays are implemented in version for command line.

--- Target/BlackBox/+Text.bat
+++ Target/BlackBox/+Text.bat
@@ -10,19 +10,19 @@ SET txt=%XDev%\Bin\odc2txt.exe
 IF errorlevel 1 PAUSE
 %txt% Mod\Cmd.odc Mod\Text\Cmd.txt
 IF errorlevel 1 PAUSE
-%txt% Mod\OPB.odc Mod\Text\OPB.txt
+%txt% Mod\OPB.odc ..\..\Mod\OfrontOPB.Mod
 IF errorlevel 1 PAUSE
-%txt% Mod\OPC.odc Mod\Text\OPC.txt
+%txt% Mod\OPC.odc ..\..\Mod\OfrontOPC.Mod
 IF errorlevel 1 PAUSE
 %txt% Mod\OPM.odc Mod\Text\OPM.txt
 IF errorlevel 1 PAUSE
-%txt% Mod\OPP.odc Mod\Text\OPP.txt
+%txt% Mod\OPP.odc ..\..\Mod\OfrontOPP.Mod
 IF errorlevel 1 PAUSE
 %txt% Mod\OPS.odc ..\..\Mod\OfrontOPS.Mod
 IF errorlevel 1 PAUSE
-%txt% Mod\OPT.odc Mod\Text\OPT.txt
+%txt% Mod\OPT.odc ..\..\Mod\OfrontOPT.Mod
 IF errorlevel 1 PAUSE
-%txt% Mod\OPV.odc Mod\Text\OPV.txt
+%txt% Mod\OPV.odc ..\..\Mod\OfrontOPV.Mod
 IF errorlevel 1 PAUSE
 %txt% Rsrc\Strings.odc Rsrc\Text\Strings.txt
 IF errorlevel 1 PAUSE
