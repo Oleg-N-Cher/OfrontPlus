7c9fc0c9f492db58e9bb3dfb600b11d25bf0a135
Oleg N. Cher <allot@bk.ru>
2020-08-30 23:45:27 +0300

    * OdcToText.Mod => OdcToText.Ob3

--- Target/Linux_amd64/Bin/o2c.bat
+++ Target/Linux_amd64/Bin/o2c.bat
@@ -3,7 +3,7 @@ SET PATH=%CD%\..\..\Win32
 CD ..\Obj
 SET OBERON=%CD%;%CD%\..\Lib\Sym;%CD%\..\Sym;%CD%\..\..\..\Mod
 
-ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m OdcToText.Mod -3m OEF.Ob2 -2m
+ofront+ -sC -88 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m OdcToText.Ob3 -3m OEF.Ob2 -2m
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
--- Target/Win32/Bin/o2c.bat
+++ Target/Win32/Bin/o2c.bat
@@ -6,7 +6,7 @@ SET OBERON=.;..\Lib\Sym;..\Sym;..\..\..\Mod
 SET oef=
 IF EXIST ..\oef.exe SET oef=^| oef
 
-ofront+ -sC -48 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m OdcToText.Mod -3m OEF.Ob2 -2m %oef%
+ofront+ -sC -48 OfrontErrors.Mod OfrontOPM.cmdln.Mod OfrontOPS.Mod OfrontOPT.Mod OfrontOPB.Mod OfrontOPP.Mod OfrontOPC.Mod OfrontOPV.Mod BrowserCmd.Mod -m ImportGraph.Mod -m OCatCmd.Mod -m OfrontCmd.Mod -m OdcToText.Ob3 -3m OEF.Ob2 -2m %oef%
 IF errorlevel 1 PAUSE
 
 FOR %%i IN (*.sym) DO MOVE /Y %%i ..\Sym >NUL
