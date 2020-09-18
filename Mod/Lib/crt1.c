cf382b54fc18672248af40b4253eb993a9394181
Oleg N. Cher <allot@bk.ru>
2020-09-19 02:13:12 +0300

    * issue #98: getting rid of __builtin_frame_address(0) for UNIX again; removed conflict with WinMain when included <windows.h> (thanks to Stewart Greenhill).

--- Mod/Lib/crt1.c
+++ Mod/Lib/crt1.c
@@ -1,25 +1,13 @@
-#define SYSTEM_Cfg_KERNEL32
-
-int argc; char *argv; void *stkadr;
+int argc; char **argv;
 
 __declspec(dllimport) void __stdcall ExitProcess (int);
-__declspec(dllimport) void exit (int);
-__declspec(dllimport) int __getmainargs (int*, char**, char**, int, void*);
-int __stdcall WinMain (void*, void*, char*, int);
+__declspec(dllimport) int __getmainargs (int*, char***, char**, int, void*);
+extern int _WinMain (int argc, char **argv, void *stkadr);
 
-/*
- * For now the GUI startup function is the same as the console one.
- * This simply gets rid of the annoying warning about not being able
- * to find WinMainCRTStartup when linking GUI applications.
- */
-void _WinMain (void) {
-  char* tmp; __getmainargs(&argc, &argv, &tmp, 0, &tmp);
-  stkadr = __builtin_frame_address(0);
-#ifdef SYSTEM_Cfg_KERNEL32
+void WinMain (void) {
+  char* tmp;
+  __getmainargs(&argc, &argv, &tmp, 0, &tmp);
   ExitProcess(
-#else //SYSTEM_Cfg_MSVCRT
-  exit(
-#endif
-    WinMain((void*)0, (void*)0, (char*)0, 0)
+    _WinMain(argc, argv, __builtin_frame_address(0))
   );
-} //_WinMain
+} //WinMain
