5ff2cdf2e0d916784fc82b5a9dcb73f128c17688
Oleg N. Cher <allot@bk.ru>
2022-01-16 23:23:18 +0200

    * crt1.c

--- Mod/Lib/crt1.c
+++ Mod/Lib/crt1.c
@@ -1,5 +1,8 @@
 extern int _main (void *stktop);
+__attribute__((dllimport)) void __stdcall ExitProcess (int uExitCode);
 
-int WinMain (void) {
-  return _main(__builtin_frame_address(0));
+void __stdcall WinMain (
+  void *hInstance, void *hPrevInstance, char *lpCmdLine, int nCmdShow)
+{
+  ExitProcess(_main(__builtin_frame_address(0)));
 } //WinMain
